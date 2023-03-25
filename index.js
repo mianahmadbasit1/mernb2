332af5783d44dffd5eed0ae3f962f0b544f086b6
Mian Ahmad Basit <66495447+mianahmadbasit1@users.noreply.github.com>
2023-03-25 16:09:55 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+alert("hi ahmad");
+
+
+
+var b=confirm("are you ahmad");
+console.log(b)
+var a = prompt("enter your age");
+alert("Dear your age is "  + a);
+console.log("Dear your age is "  + a)
+document.write("Dear your age is "  + a)
